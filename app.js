beb0bfa83ed6a2b45c12976b1f7cd4c0f93f6587
wingraptor <akonobrathwaite@gmail.com>
2018-12-07 20:24:39 -0400

    Add home route

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const url =
   "https://www.complex.com/music/best-albums-2018/mariah-carey-caution";
 
 //Spotify config
-const client_id = "624b38195d6f486f9b290e99fc6e8b91"; // Spotify client id
+const my_client_id = "624b38195d6f486f9b290e99fc6e8b91"; // Spotify client id
 const client_secret = "84ebfb41b6414d5095323ec09a7aa8a6"; // Spotify secret
 const accessToken =
-  "BQCAMlMUonAx-k-_pSttqVKxXG5TJsGOiejfgT7MB6tRjRK5Qm7FmPpPbVQH4T9lN8NvjoNbGv8SLqvrOiDY3HAOelLUnkS9UPbhk4obX9IKaVrxIeM2t5sY-1i_uTruy8eP1AIle7HgXiC8PIQwZSKb5cUHMqqL9g2AU8ayp7YSJN0"; //Spotify access token
+  //Spotify access token
+  "BQCAMlMUonAx-k-_pSttqVKxXG5TJsGOiejfgT7MB6tRjRK5Qm7FmPpPbVQH4T9lN8NvjoNbGv8SLqvrOiDY3HAOelLUnkS9UPbhk4obX9IKaVrxIeM2t5sY-1i_uTruy8eP1AIle7HgXiC8PIQwZSKb5cUHMqqL9g2AU8ayp7YSJN0";
 const albumArtistArr = [];
 
 //mongoose config
@@ -29,111 +30,111 @@ var albumSchema = new mongoose.Schema({
 var Album = mongoose.model("Album", albumSchema);
 
 // Parse data from website to get list of albums to add to Album collection - https://www.complex.com/music/best-albums-2018
-puppeteer
-  .launch()
-  .then(function(browser) {
-    return browser.newPage();
-  })
-  .then(function(page) {
-    return page.goto(url).then(function() {
-      return page.content();
-    });
-  })
-  .then(function(html) {
-    //Select markup on page containing artist and album names.
-    const albumArtistHTML = $(".list-slide__title", html);
-    //Add artist, album and position of each album to an array
-    albumArtistHTML.each(function() {
-      albumArtistArr.push($(this).text());
-    });
-    // Extract album names and add to database - https://stackoverflow.com/questions/12367126/how-can-i-get-a-substring-located-between-2-quotes
-    albumArtistArr.forEach(function(e) {
-      Album.create({
-        name: e.match(/‘([^']+)’/)[1]
-      });
-    });
-  })
-  .catch(function(err) {
-    //handle error
-    console.log(err);
-  });
+// puppeteer
+//   .launch()
+//   .then(function(browser) {
+//     return browser.newPage();
+//   })
+//   .then(function(page) {
+//     return page.goto(url).then(function() {
+//       return page.content();
+//     });
+//   })
+//   .then(function(html) {
+//     //Select markup on page containing artist and album names.
+//     const albumArtistHTML = $(".list-slide__title", html);
+//     //Add artist, album and position of each album to an array
+//     albumArtistHTML.each(function() {
+//       albumArtistArr.push($(this).text());
+//     });
+//     // Extract album names and add to database - https://stackoverflow.com/questions/12367126/how-can-i-get-a-substring-located-between-2-quotes
+//     albumArtistArr.forEach(function(e) {
+//       Album.create({
+//         name: e.match(/‘([^']+)’/)[1]
+//       });
+//     });
+//   })
+//   .catch(function(err) {
+//     //handle error
+//     console.log(err);
+//   });
 
 
-var options = {
-  url: `https://api.spotify.com/v1/search?q=${albums[i].name}&type=album&market=US&limit=1&offset=0`,
-  headers: {
-    Authorization: "Bearer " + accessToken
-  },
-  json: true // Automatically stringifies the body to JSON
-};
+// var options = {
+//   url: `https://api.spotify.com/v1/search?q=${albums[i].name}&type=album&market=US&limit=1&offset=0`,
+//   headers: {
+//     Authorization: "Bearer " + accessToken
+//   },
+//   json: true // Automatically stringifies the body to JSON
+// };
 
-//Add Spotify Album IDs and artist names to album documents
-Album.find({}, function(err, albums) {
-  if (err) {
-    console.log(err);
-  } else {
-    //Iterate through the array of queried albums
-    for (var i = 0; i < albums.length; i++) {
-      //Update url to match 
-      options.url = `https://api.spotify.com/v1/search?q=${albums[i].name}&type=album&market=US&limit=1&offset=0`
-      //Send search request to spotify for each album - options.url
-      request.get(options, function(error, response, body) {
-        //Search for specific album and add album id and artist name received from Spotify query
-        Album.findOneAndUpdate(
-          { name: /body.albums.items[0].name/i},
-          {
-            album_id: body.albums.items[0].id,
-            artist: body.albums.items[0].artists[0].name
-          },
-          function(err) {
-            if (err) {
-              console.log(err);
-            }
-            else {
-              console.log("Album data added successfully");
-            }
-          }
-        );
-      });
-    }
-  }
-});
+// //Add Spotify Album IDs and artist names to album documents
+// Album.find({}, function(err, albums) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     //Iterate through the array of queried albums
+//     for (var i = 0; i < albums.length; i++) {
+//       //Update url to match 
+//       options.url = `https://api.spotify.com/v1/search?q=${albums[i].name}&type=album&market=US&limit=1&offset=0`
+//       //Send search request to spotify for each album - options.url
+//       request.get(options, function(error, response, body) {
+//         //Search for specific album and add album id and artist name received from Spotify query
+//         Album.findOneAndUpdate(
+//           { name: /body.albums.items[0].name/i},
+//           {
+//             album_id: body.albums.items[0].id,
+//             artist: body.albums.items[0].artists[0].name
+//           },
+//           function(err) {
+//             if (err) {
+//               console.log(err);
+//             }
+//             else {
+//               console.log("Album data added successfully");
+//             }
+//           }
+//         );
+//       });
+//     }
+//   }
+// });
 
-// Add tracklists to album documents
-Album.find({}, function(err, albums) {
-  if (err) {
-    console.log(err);
-  } else {
-    //Iterate through the array of queried albums
-    for (var i = 0; i < albums.length; i++) {
-      //Construct URL to get album's tracklist
-      options.url = `https://api.spotify.com/v1/albums/${albums[i].album_id}/tracks`;
-      //Send request to spotify for tracklist of album
-      request.get(options, function(error, response, body) {
-        //Generate array of track names, ids and track numbers
-        var tracklist = [];
-        for (var j = 0; j < body.items.length; j++){
-          tracklist[j] = {
-            track_id: body.items[j].id,
-            name: body.items[j].name,
-            track_number: body.items[j].track_number
-          }
-        };
-        //Update found album with album id and artist name from Spotify
-        Album.findOneAndUpdate(
-          { artist: body.items[0].artists[0].name },
-          {
-            tracklist: tracklist
-          },
-          function(err, albums) {
-            if (err) {
-              console.log(err);
-            }
-          }
-        );
-      });
-    }
-  }
-});
+// // Add tracklists to album documents
+// Album.find({}, function(err, albums) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     //Iterate through the array of queried albums
+//     for (var i = 0; i < albums.length; i++) {
+//       //Construct URL to get album's tracklist
+//       options.url = `https://api.spotify.com/v1/albums/${albums[i].album_id}/tracks`;
+//       //Send request to spotify for tracklist of album
+//       request.get(options, function(error, response, body) {
+//         //Generate array of track names, ids and track numbers
+//         var tracklist = [];
+//         for (var j = 0; j < body.items.length; j++){
+//           tracklist[j] = {
+//             track_id: body.items[j].id,
+//             name: body.items[j].name,
+//             track_number: body.items[j].track_number
+//           }
+//         };
+//         //Update found album with album id and artist name from Spotify
+//         Album.findOneAndUpdate(
+//           { artist: body.items[0].artists[0].name },
+//           {
+//             tracklist: tracklist
+//           },
+//           function(err, albums) {
+//             if (err) {
+//               console.log(err);
+//             }
+//           }
+//         );
+//       });
+//     }
+//   }
+// });
 
 module.exports = Album;
--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const request = require("request");
 const rp = require("request-promise");
 const mongoose = require("mongoose");
+const ejs = require("ejs");
+const express = require("express");
 const Album = require("./app.js");
 
 //mongoose config
@@ -9,9 +11,17 @@ mongoose.connect(
   { useNewUrlParser: true }
 );
 
+//EJS + Express config
+var app = express();
+app.set("view engine", "ejs");
+
 const name = "Hello";
-const accessToken =
-  "BQDdX7Te4boDgO66Kg_Qy0HdCgL-7CxEMztIuJYWY4qBkb_uLgWDjRjHtUdkGb_Kgj9w652EjUtSdb_Yy8P-q4rCI5y8IdTtlpAeTwko7OtiC8oHc0a_4tpbIh6wxY4X2Wuczd0do6kJSUm1gQOrDXiha0I4pbdsHZ7jSH75yhvchyVxTtjwEk5BgPBG_q57Od0oKA2mlF-puIDAMetN9jM";
+
+//Spotify config
+const my_client_id = "624b38195d6f486f9b290e99fc6e8b91"; // Spotify client id
+const client_secret = "84ebfb41b6414d5095323ec09a7aa8a6"; // Spotify secret
+const accessToken = "";
+const redirect_uri = "http://localhost:3000/";
 
   // Initial options to get user's Spotify ID
 const options = {
@@ -22,33 +32,51 @@ const options = {
   json: true // Automatically stringifies the body to JSON
 };
 
-/* --------------------------------------------------------
+
+app.get("/", function(req,res){
+  res.render("home.ejs");
+});
+
+app.get("/login", function(req,res){
+  var scopes = "playlist-modify-public playlist-modify-private user-read-private";
+  res.redirect('https://accounts.spotify.com/authorize' +
+    '?response_type=code' +
+    '&client_id=' + my_client_id +
+    (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
+    '&redirect_uri=' + encodeURIComponent(redirect_uri));
+});
+
+app.get("post", function (req, res) {
+  res.render("home.ejs");
+});
+
+/*--------------------------------------------------------
 
 https://github.com/request/request-promise - for more info.
 
------------------------------------------------------------ */
+-----------------------------------------------------------*/
 // Get UserID
-rp(options)
-  .then(function(body) {
-    // Update options object
-    options.url = `https://api.spotify.com/v1/users/${body.id}/playlists`;
-    options.method = "POST";
-    options.body = {
-      name: name,
-      description:
-        "Songs from the best 50 albums of 2018 according to Complex Mag.",
-      public: false
-    };
-    // Create new playlist
-    rp(options).then(function(body) {
-      // Update options object
-      options.url = `https://api.spotify.com/v1/playlists/${body.id}/tracks`;
-      uriConstructor();
-    });
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
+// rp(options)
+//   .then(function(body) {
+//     // Update options object
+//     options.url = `https://api.spotify.com/v1/users/${body.id}/playlists`;
+//     options.method = "POST";
+//     options.body = {
+//       name: name,
+//       description:
+//         "Songs from the best 50 albums of 2018 according to Complex Mag.",
+//       public: false
+//     };
+//     // Create new playlist
+//     rp(options).then(function(body) {
+//       // Update options object
+//       options.url = `https://api.spotify.com/v1/playlists/${body.id}/tracks`;
+//       uriConstructor();
+//     });
+//   })
+//   .catch(function(err) {
+//     console.log(err);
+//   });
 
 
 /*-------------------------------------------------------------------------------------------------------
@@ -57,39 +85,47 @@ Queries DB for trackIDs and constructs an array of track IDs (uris)
 see here: https://developer.spotify.com/documentation/web-api/reference/playlists/add-tracks-to-playlist/
 -------------------------------------------------------------------------------------------------------- */
 
-function uriConstructor() {
-  Album.find({}, "tracklist.track_id", function(err, val) {
-    var count = 0;
-    var tracksArray = [];
+// function uriConstructor() {
+//   Album.find({}, "tracklist.track_id", function(err, val) {
+//     var count = 0;
+//     var tracksArray = [];
 
-    for (var i = 0; i < val.length; i++) {
-      for (var j = 0; j < val[i].tracklist.length; j++) {
-        tracksArray.push(`spotify:track:${val[i].tracklist[j].track_id}`);
-        count++;
-        // Allow batches of 100 songs to be added to playlist
-        if (count === 100) {
-          addTracks(tracksArray);
-          // Prepare for a new batch of tracks to be created
-          tracksArray = [];
-          count = 0;
-        }
-      }
-    }
-    // Allows batches of smaller than 100 tracks to be added to playlist
-    addTracks(tracksArray);
-  });
-}
+//     for (var i = 0; i < val.length; i++) {
+//       for (var j = 0; j < val[i].tracklist.length; j++) {
+//         tracksArray.push(`spotify:track:${val[i].tracklist[j].track_id}`);
+//         count++;
+//         // Allow batches of 100 songs to be added to playlist
+//         if (count === 100) {
+//           addTracks(tracksArray);
+//           // Prepare for a new batch of tracks to be created
+//           tracksArray = [];
+//           count = 0;
+//         }
+//       }
+//     }
+//     // Allows batches of smaller than 100 tracks to be added to playlist
+//     addTracks(tracksArray);
+//   });
+// }
 
-// Add batches of songs to Spotify playlist
-function addTracks(arr, count) {
-  // Add array to request body
-  options.body = { uris: arr };
-  // send request to add tracks to playlist
-  rp(options)
-    .then(function(body) {
-      console.log("batch successfully added");
-  })
-    .catch(function(err){
-      console.log(`Error adding tracks to playlist: ${err}`);
-    })
-}
+// // Add batches of songs to Spotify playlist
+// function addTracks(arr, count) {
+//   // Add array to request body
+//   options.body = { uris: arr };
+//   // send request to add tracks to playlist
+//   rp(options)
+//     .then(function(body) {
+//       console.log("batch successfully added");
+//   })
+//     .catch(function(err){
+//       console.log(`Error adding tracks to playlist: ${err}`);
+//     });
+// }
+
+
+//Tell Express to listen for requests on port 3000 (starts local server)
+//Visit localhost:3000 to reach site being served by local server.
+app.listen(3000, function () {
+  //Logs "Server has started" to the console when server has been started
+  console.log("Server has started");
+});
